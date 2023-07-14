a351c530e6244d47e919ae15e564222590002b4e
Michael Donato Murphy <sunshine.cid@gmail.com>
2023-07-14 12:39:29 -0600

    Made script looping
    Removed unnecessary import, added oauth comment

--- redditbot.py
+++ redditbot.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 import os
 import praw
-import pdb
 import re
+import time
 
 #This should pull your setting from the [bot1] section of your praw.ini
 #NOTE: Currently cannot get praw.ini to work
 #reddit = praw.Reddit("bot1")
 
 #Now it should include your refresh token which should let us post from the bot.
+#If you've no idea what's going on here refer to the OAuth subdirectory, 
+# and the oauth*.py steps for PRAW and Reddit authentication.
 reddit = praw.Reddit(
     username="",
     client_id="",
@@ -20,32 +22,38 @@ reddit = praw.Reddit(
 
 subreddit = reddit.subreddit("ShadowBanned")
 
-# Check if a post_reply_history.txt file exists
-# If not, make it, If yes filter out previously replied to posts
-# Thanks to https://github.com/shantnu for the majority of this code
-if not os.path.isfile("post_reply_history.txt"):
-    posts_replied_to = []
-else:
-    # Read the file into a list and remove any empty values
-    with open("post_reply_history.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
+while True:
+    print("ShadowBanBot running - press [CTRL]-C to Exit")
 
-for submission in subreddit.new(limit=29):
-    # If we haven't replied to this post before
-    if submission.id not in posts_replied_to:
+    # Check if a post_reply_history.txt file exists
+    # If not, make it, If yes filter out previously replied to posts
+    # Thanks to https://github.com/shantnu for the majority of this code
+    if not os.path.isfile("post_reply_history.txt"):
+        post_reply_history = []
+    else:
+        # Read the file into a list and remove any empty values
+        with open("post_reply_history.txt", "r") as f:
+            post_reply_history = f.read()
+            post_reply_history = post_reply_history.split("\n")
+            post_reply_history = list(filter(None, post_reply_history))
 
-        # Do a case insensitive search for any_non_whitespace_char + anything_wildcard
-        if re.search("\S?", submission.title, re.IGNORECASE):
-            # Reply to the post
-            submission.reply("This bot can read your post. Which ultimately means you're NOT shadow banned. Be aware though, If you have low karma (it varies by subreddit) it may impact you posting in other places. Again, this action was performed by a bot. If you reply to this message you may not get a response.")
-            print("Bot replying to : ", submission.title)
+    for submission in subreddit.new(limit=29):
+        # If we haven't replied to this post before
+        if submission.id not in post_reply_history:
 
-            # Store the current id into our list
-            posts_replied_to.append(submission.id)
+            # Do a case insensitive search for any_char + anything_repeated
+            if re.search(".?", submission.title, re.IGNORECASE):
+                # Reply to the post
+                submission.reply("This bot can read your post. Which ultimately means you're NOT shadow banned. Be aware though, If you have low karma (it varies by subreddit) it may impact you posting in other places. Again, this action was performed by a bot. If you reply to this message you may not get a response.")
+                print("Bot replying to : ", submission.title)
 
-# Write our updated list back to the file
-with open("post_reply_history.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
+                # Store the current id into our list
+                post_reply_history.append(submission.id)
+
+    # Write our updated list back to the file
+    with open("post_reply_history.txt", "w") as f:
+        for post_id in post_reply_history:
+            f.write(post_id + "\n")
+
+    #Wait 5 minutes between loops
+    time.sleep(300)
