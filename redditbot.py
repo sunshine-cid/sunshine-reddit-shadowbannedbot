cec2061522e28efe379bc6110055ed01d4e4782a
Michael Donato Murphy <sunshine.cid@gmail.com>
2023-07-19 15:55:23 -0600

    Bringing everything inline with pep8
    specified utf-8 encoding, PROPPER_VARIABLE_NAMES, correct import order, and """proper opening description"""

--- redditbot.py
+++ redditbot.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
+"""This reddit bot is designed to automatically reply to every post visible
+in /r/ShadowBanned. Thereby helping posters know that theye are not shadow
+banned."""
 import os
-import praw
 import re
 import time
+import praw
+
 
 #This should pull your setting from the [bot1] section of your praw.ini
 #NOTE: Currently cannot get praw.ini to work
 #reddit = praw.Reddit("bot1")
 
 #Now it should include your refresh token which should let us post from the bot.
-#If you've no idea what's going on here refer to the OAuth subdirectory, 
+#If you've no idea what's going on here refer to the OAuth subdirectory,
 #and the oauth*.py steps for PRAW and Reddit authentication.
 reddit = praw.Reddit(
     username="",
@@ -21,7 +25,7 @@ reddit = praw.Reddit(
 )
 
 #This is the message which will be submitted by the bot as a reply
-bot_message = ("Beep, boop. This bot can read your post."
+BOT_MESSAGE = ("Beep, boop. This bot can read your post. "
                "Which ultimately means you're NOT shadow banned. Be aware "
                "though, If you have low karma it may impact you posting "
                "in other places. What is considered *low* varies by subreddit."
@@ -29,7 +33,7 @@ bot_message = ("Beep, boop. This bot can read your post."
                "If you reply to this message you may not get a response.")
 
 #This is the time to sleep, in seconds, between while-loop cycles
-sleep_time = 300 #300 is 5 minutes
+SLEEP_TIME = 300 #300 is 5 minutes
 
 subreddit = reddit.subreddit("ShadowBanned")
 
@@ -43,7 +47,7 @@ while True:
         post_reply_history = []
     else:
         # Read the file into a list and remove any empty values
-        with open("post_reply_history.txt", "r") as f:
+        with open("post_reply_history.txt", "r", encoding="utf-8") as f:
             post_reply_history = f.read()
             post_reply_history = post_reply_history.split("\n")
             post_reply_history = list(filter(None, post_reply_history))
@@ -55,16 +59,17 @@ while True:
             # Do a case insensitive search for any_char + anything_repeated
             if re.search(".?", submission.title, re.IGNORECASE):
                 # Reply to the post
-                submission.reply(bot_message)
+                submission.reply(BOT_MESSAGE)
                 print("Bot replying to : ", submission.title)
 
                 # Store the current id into our list
                 post_reply_history.append(submission.id)
 
     # Write our updated list back to the file
-    with open("post_reply_history.txt", "w") as f:
+    with open("post_reply_history.txt", "w", encoding="utf-8") as f:
         for post_id in post_reply_history:
             f.write(post_id + "\n")
 
     #Wait between loops
-    time.sleep(sleep_time)
+    time.sleep(SLEEP_TIME)
+    
